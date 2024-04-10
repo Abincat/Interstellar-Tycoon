900471561fe5c3b9e1fe1f3479734ae0bddfe05d
LAPTOP-TQNBF0M8\ssdrive <cpeter618@gmail.com>
2024-04-10 21:47:00 +0800

    header,footer review

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,140 +1,151 @@
-
 <template>
-    <footer>
-        <main>
-            <div class="information">
-                <img src="../assets/images/logo-new.png" alt="logoImg">
-                <ul class="informationText">
-                    <li>{{ name }}</li>
-                    <li>{{ tel }}</li>
-                    <li>{{ address }}</li>
-                </ul>
-            </div>
-            <nav>
-                <div class="community">
-                    <a href="#" class="facebookHover">
-                        <img src="../assets/images/facebook-white.png" alt="facebook">
-                        <img src="../assets/images/facebook-yellow.png" alt="facebookYellow" class="facebookYellow">
-                    </a>
-                    <a href="#" class="instagramHover">
-                        <img src="../assets/images/instagram-white.png" alt="instagram">
-                        <img src="../assets/images/instagram-yellow.png" alt="instagramYellow" class="instagramYellow">
-                    </a>
-                    <a href="#" class="youtubeHover">
-                        <img src="../assets/images/youtube-white.png" alt="youtube">
-                        <img src="../assets/images/youtube-yellow.png" alt="youtubeYellow" class="youtubeYellow">
-                    </a>
-                </div>
-                <div class="linkList">
-                    <div>
-                        <router-link to="introduce" class="linkButton">introduce</router-link>
-                    </div>
-                    <div>
-                        <router-link to="reserve" class="linkButton">reserve</router-link>
-                    </div>
-                </div>
-            </nav>
-        </main>
-    </footer>
-    
+  <footer>
+    <!-- 為甚麼footer裡面要再放main footer就已經不會是你的主要內容了 去MDN查一下main標籤怎麼用 -->
+    <main>
+      <div class="information">
+        <img src="../assets/images/logo-new.png" alt="logoImg" />
+        <ul class="informationText">
+          <li>{{ name }}</li>
+          <li>{{ tel }}</li>
+          <li>{{ address }}</li>
+        </ul>
+      </div>
+      <nav>
+        <div class="community">
+          <a href="#" class="facebookHover">
+            <img src="../assets/images/facebook-white.png" alt="facebook" />
+            <img
+              src="../assets/images/facebook-yellow.png"
+              alt="facebookYellow"
+              class="facebookYellow"
+            />
+          </a>
+          <a href="#" class="instagramHover">
+            <img src="../assets/images/instagram-white.png" alt="instagram" />
+            <img
+              src="../assets/images/instagram-yellow.png"
+              alt="instagramYellow"
+              class="instagramYellow"
+            />
+          </a>
+          <a href="#" class="youtubeHover">
+            <img src="../assets/images/youtube-white.png" alt="youtube" />
+            <img
+              src="../assets/images/youtube-yellow.png"
+              alt="youtubeYellow"
+              class="youtubeYellow"
+            />
+          </a>
+        </div>
+        <div class="linkList">
+          <!-- 跟header一樣 div是否多餘-->
+          <div>
+            <router-link to="introduce" class="linkButton"
+              >introduce</router-link
+            >
+          </div>
+          <div>
+            <router-link to="reserve" class="linkButton">reserve</router-link>
+          </div>
+        </div>
+      </nav>
+    </main>
+  </footer>
 </template>
 
 <script>
-export default{
-    data(){
-        return{
-            name:"星際大亨",
-            tel:"(02)0000-1234",
-            address:"408台中市南屯區春安路56巷25號",
-        };
-    }
+export default {
+  data() {
+    return {
+      name: "星際大亨",
+      tel: "(02)0000-1234",
+      address: "408台中市南屯區春安路56巷25號",
+    };
+  },
 };
 </script>
 
 <style scoped>
-    footer{
-        display: flex;
-        justify-content: center;
-    }
+footer {
+  display: flex;
+  justify-content: center;
+}
 
-    main{
-        display: flex;
-        width: 80%;
-        justify-content:space-between;
-    }
+main {
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+}
 
-    .information{
-        display: flex;
-        align-items: center;
-    }
+.information {
+  display: flex;
+  align-items: center;
+}
 
-    .informationText{
-        line-height: 1.5em;
-        padding: 0 10px
-    }
+.informationText {
+  line-height: 1.5em;
+  padding: 0 10px;
+}
 
-    img{
-        width: 80px;
-        height: 80px; 
-    }
+img {
+  width: 80px;
+  height: 80px;
+}
 
-    nav{
-        display: block;
-    }
+nav {
+  display: block;
+}
 
-    .community{
-        display: flex;
-        justify-content: center;
-    }
+.community {
+  display: flex;
+  justify-content: center;
+}
 
-    .community img{
-        width: 40px;
-        height: 40px;
-        padding: 10px;
-        border-radius:50% ; 
-    }
+.community img {
+  width: 40px;
+  height: 40px;
+  padding: 10px;
+  border-radius: 50%;
+}
 
-    .linkList{
-        display: flex;
-        margin: 10px;
-    }
+.linkList {
+  display: flex;
+  margin: 10px;
+}
 
-    .linkButton{
-        text-decoration: none;
-        color:white;
-        margin: 0 20px;
-    }
+.linkButton {
+  text-decoration: none;
+  color: white;
+  margin: 0 20px;
+}
 
-    .linkButton:hover{
-        color:rgb(255, 208, 78) ;
-    }
+.linkButton:hover {
+  color: rgb(255, 208, 78);
+}
 
-    li{
-        display: block;
-        font-size: 12px;
-    }
+li {
+  display: block;
+  font-size: 12px;
+}
 
-    .facebookHover,
-    .instagramHover,
-    .youtubeHover
-    {
-        position: relative;
-    }
+.facebookHover,
+.instagramHover,
+.youtubeHover {
+  position: relative;
+}
 
-    .facebookYellow,
-    .instagramYellow,
-    .youtubeYellow
-    {
-        position: absolute;
-        top:0;
-        left: 0;
-        opacity: 0;
-    }
+.facebookYellow,
+.instagramYellow,
+.youtubeYellow {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+}
 
-    .facebookYellow:hover,
-    .instagramYellow:hover,
-    .youtubeYellow:hover
-    {
-        opacity: 1;
-    }
+.facebookYellow:hover,
+.instagramYellow:hover,
+.youtubeYellow:hover {
+  opacity: 1;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,65 +1,66 @@
-
 <template>
-    <nav>
-        <router-link to="/" class="logo">
-            <img src="../assets/images/logo-new.png" alt="logoImg">
-        </router-link>
-        <div class="linkButtonList">
-            <div>
-                <router-link to="introduce" class="linkButton">introduce</router-link>
-            </div>
-            <div>
-                <router-link to="reserve" class="linkButton">reserve</router-link>
-            </div>
-        </div>
-    </nav>
+  <nav>
+    <router-link to="/" class="logo">
+      <img src="../assets/images/logo-new.png" alt="logoImg" />
+    </router-link>
+    <div class="linkButtonList">
+      <!-- 原本的多加一層 div ，沒有特別需要其實可以不用，拿掉不影響css也讓html更乾淨
+        重點是加上原本的css讓你的元素範圍變很奇怪，你的滑鼠在移動的時候要移到元素上才有hover效果
+    -->
+      <router-link to="introduce" class="linkButton">introduce</router-link>
+      <router-link to="reserve" class="linkButton">reserve</router-link>
+    </div>
+  </nav>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
-nav{
-    display:flex;
-    padding: 8px;
-    justify-content:space-between;
+nav {
+  display: flex;
+  padding: 8px;
+  justify-content: space-between;
 }
 
-.logo img{
-    width: 100px;
-    height: 100px;
+.logo img {
+  /* 高度可以不用特別設，可以吃圖片比例 */
+  width: 50px;
 }
-
+/* 
+原本的
 .linkButtonList{
     display: flex;
     justify-content: end;
     padding-top: 20px;
+}*/
+
+.linkButtonList {
+  display: flex;
+  align-self: center;
+  gap: 10px;
 }
 
-.linkButton{
-    color: white;
-    font-size: 20px;
-    margin: 0 10px;
-    padding: 10px 10px 0 10px;
-    text-decoration: none;
+.linkButton {
+  color: white;
+  font-size: 20px;
+  margin: 0 10px;
+  padding: 10px 10px 0 10px;
+  text-decoration: none;
 }
 
-.linkButton:hover{
-    background: linear-gradient(to bottom, #ffffff00 , rgb(255,208,78,0.5));
-    border-bottom:3px solid white ;
-    animation: navHover 0.8s;
+.linkButton:hover {
+  background: linear-gradient(to bottom, #ffffff00, rgb(255, 208, 78, 0.5));
+  border-bottom: 3px solid white;
+  animation: navHover 0.8s;
 }
 
-@keyframes navHover{
-    0%{
-        opacity: 0;
-    }
+@keyframes navHover {
+  0% {
+    opacity: 0;
+  }
 
-    100%{
-        opacity: 1;
-    }
+  100% {
+    opacity: 1;
+  }
 }
-
-
 </style>
